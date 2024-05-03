7d6cb85e5c2922d4555dbfac601e861bdfb72510
Farhan Tariq Banday <167176424+bandayfarhan9@users.noreply.github.com>
2024-05-03 10:54:25 +0530

    madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -7,11 +7,3 @@ famous_person = input("famous person:")
 madlib = f"computer programming is so {adj}! It makes me excited all the time because I love to {verb1}. Stay hydrated and {verb2}like you are a {famous_person}!"
 print(madlib)
 
-
-Adj = ("Adjective :")
-Verb1 = ("Verb1 : ")
-Verb2 = ("Verb2")
-famous_person = ("Famous person")
-
-madlib = f""
-print(madlib)
