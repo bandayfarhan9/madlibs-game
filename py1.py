f536417b173457fb00e5b2da93fb5e07ffad5485
Farhan Tariq Banday <167176424+bandayfarhan9@users.noreply.github.com>
2024-05-03 10:53:12 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,17 @@
+adj = input("Adjecive :")
+verb1 = input("verb1 :")
+verb2 = input("verb2 :")
+famous_person = input("famous person:")
+
+
+madlib = f"computer programming is so {adj}! It makes me excited all the time because I love to {verb1}. Stay hydrated and {verb2}like you are a {famous_person}!"
+print(madlib)
+
+
+Adj = ("Adjective :")
+Verb1 = ("Verb1 : ")
+Verb2 = ("Verb2")
+famous_person = ("Famous person")
+
+madlib = f""
+print(madlib)
